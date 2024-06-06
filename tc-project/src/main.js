20323eacd5ac912a87b782bc0c4ed50989a247f5
Sangwoo Byun <bsw@cntt.co.kr>
2024-06-06 11:40:11 +0900

    추가 수정

--- tc-project/src/main.js
+++ tc-project/src/main.js
@@ -1,7 +1,287 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "../router/router";
+import router from "./router/router";
 
 const app = createApp(App);
 app.use(router);
-app.mount("#app");
+// 기존 Vue 인스턴스 제거
+const appElement = document.getElementById("app");
+if (appElement.__vue_app__) {
+    appElement.__vue_app__.unmount();
+}
+// 새 Vue 인스턴스 마운트
+app.mount(appElement);
+
+export const commonUi = {
+    init: function () {
+        this.inputHandlers();
+        this.commonGnb();
+    },
+
+    // 헤더 고정
+
+    // input 수정 소스
+    inputHandlers: function () {
+        const InputBox = document.querySelectorAll(".input-box");
+        const InputDel = document.querySelectorAll(".input-del");
+        const showToggle = document.querySelectorAll(".show-toggle");
+        const customChk = document.querySelectorAll(
+            ".radio input, .checkbox input"
+        );
+        // 인풋 readonly 및 disabled 추가
+        function handleInputElements(selector, parentClass) {
+            const inputs = document.querySelectorAll(selector);
+
+            inputs.forEach((input) => {
+                const parentDiv = input.parentElement;
+                parentDiv.classList.add(parentClass);
+
+                const elementsToHide = parentDiv.querySelectorAll(".input-del");
+                elementsToHide.forEach((element) => {
+                    element.setAttribute("tabindex", "-1");
+                    element.classList.add("hide");
+                    element.blur();
+                });
+            });
+        }
+
+        handleInputElements("input[readonly]", "readonly");
+        handleInputElements("input[disabled]", "disabled");
+
+        const updateInputBoxState = (inputBoxDiv, inputElement) => {
+            if (inputElement.value.trim() !== "") {
+                inputBoxDiv.classList.add("hasValue");
+                setElementsTabIndex(inputBoxDiv, "0");
+            } else {
+                inputBoxDiv.classList.remove("hasValue");
+                setElementsTabIndex(inputBoxDiv, "-1");
+            }
+
+            if (inputElement.hasAttribute("readonly")) {
+                inputBoxDiv.classList.add("readOnly");
+            } else {
+                inputBoxDiv.classList.remove("readOnly");
+            }
+        };
+
+        const setElementsTabIndex = (inputBoxDiv, index) => {
+            const elementsToHide = inputBoxDiv.querySelectorAll(".input-del");
+            elementsToHide.forEach((element) => {
+                element.setAttribute("tabindex", index);
+            });
+        };
+
+        InputBox.forEach((inputBoxDiv) => {
+            const inputElement = inputBoxDiv.querySelector("input");
+            if (inputElement) {
+                inputElement.addEventListener("focus", () => {
+                    inputBoxDiv.classList.add("focus");
+                });
+                inputElement.addEventListener("blur", () => {
+                    inputBoxDiv.classList.remove("focus");
+                });
+
+                updateInputBoxState(inputBoxDiv, inputElement);
+
+                inputElement.addEventListener("input", () => {
+                    updateInputBoxState(inputBoxDiv, inputElement);
+                });
+            }
+        });
+
+        InputDel.forEach((btn, index) => {
+            btn.addEventListener("click", () => {
+                const inputBoxDiv =
+                    document.querySelectorAll(".input-box")[index];
+                const inputElement = inputBoxDiv.querySelector("input");
+                if (inputElement) {
+                    inputElement.value = "";
+                    inputBoxDiv.classList.remove("hasValue");
+                    //btn.setAttribute('tabindex', '-1')
+                }
+            });
+        });
+
+        showToggle.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                const inputElement = btn
+                    .closest(".input-box")
+                    .querySelector("input");
+                if (inputElement.type === "password") {
+                    inputElement.type = "text";
+                    btn.setAttribute("aria-label", "비밀번호 보이기");
+                    btn.classList.remove("off");
+                    btn.classList.add("on");
+                } else {
+                    inputElement.type = "password";
+                    btn.setAttribute("aria-label", "비밀번호 숨기기");
+                    btn.classList.remove("on");
+                    btn.classList.add("off");
+                }
+            });
+        });
+
+        customChk.forEach((input) => {
+            input.addEventListener("change", () => {
+                const label = input.closest("label");
+                const siblingLabels = label.querySelectorAll(".lbl");
+                siblingLabels.forEach((lbl) =>
+                    lbl.classList.add("focus-visible")
+                );
+
+                const statusInfo = label.querySelector(".status-info");
+                if (statusInfo) {
+                    if (input.checked) {
+                        statusInfo.textContent = "선택됨";
+                    } else {
+                        statusInfo.textContent = "선택안됨";
+                    }
+                }
+            });
+            input.addEventListener("focus", () => {
+                const siblingLabels = input
+                    .closest("label")
+                    .querySelectorAll(".lbl");
+                siblingLabels.forEach((label) =>
+                    label.classList.add("focus-visible")
+                );
+            });
+
+            input.addEventListener("blur", () => {
+                const siblingLabels = input
+                    .closest("label")
+                    .querySelectorAll(".lbl");
+                siblingLabels.forEach((label) =>
+                    label.classList.remove("focus-visible")
+                );
+            });
+        });
+    },
+};
+
+export function toggleGnb() {
+    const gnbOpen = document.body.classList.contains("gnbOpen");
+    const menuButton = document.querySelector(".btn-menu");
+    document.body.classList.toggle("gnbOpen");
+    if (menuButton) {
+        const newLabel = gnbOpen ? "메뉴 닫기" : "메뉴 열기";
+        menuButton.setAttribute("aria-label", newLabel);
+    }
+}
+
+// 클래스를 추가/제거 공통 스크립트
+function toggleClass(element, className, add) {
+    if (element) {
+        if (add) {
+            element.classList.add(className);
+        } else {
+            element.classList.remove(className);
+        }
+    }
+}
+
+// 팝업 열기&닫기
+export function togglePop(elementId, shouldShow, isTooltip = false) {
+    const modal = document.getElementById(elementId);
+    const triggerElement = document.querySelector(
+        `[aria-controls="${elementId}"]`
+    );
+
+    if (shouldShow) {
+        if (!isTooltip) {
+            toggleClass(document.body, "lock", true);
+        }
+        toggleClass(modal, "show", true);
+        setTimeout(function () {
+            toggleClass(modal, "show-end", true);
+            modal.setAttribute("aria-hidden", "false");
+
+            if (triggerElement) {
+                triggerElement.setAttribute("aria-expanded", "true");
+            }
+            const focusableElements = modal.querySelectorAll(
+                'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+            );
+            if (focusableElements.length) {
+                focusableElements[0].focus();
+            } else {
+                modal.focus();
+            }
+        }, 100);
+    } else {
+        if (modal.classList.contains("show-end")) {
+            if (!isTooltip) {
+                toggleClass(document.body, "lock", false);
+            }
+            modal.classList.remove("show-end");
+            modal.setAttribute("aria-hidden", "true");
+            setTimeout(function () {
+                toggleClass(modal, "show", false);
+                if (triggerElement) {
+                    triggerElement.setAttribute("aria-expanded", "false");
+                    triggerElement.focus(); // 포커스를 트리거 버튼으로 이동
+                }
+            }, 500);
+        }
+    }
+}
+
+// 전체 동의 체크박스
+app.directive("toggle-checked", {
+    beforeMount(el) {
+        const input = el.querySelector('input[type="checkbox"]');
+        if (input) {
+            input.addEventListener("change", () => {
+                if (input.checked) {
+                    el.classList.add("checked");
+                } else {
+                    el.classList.remove("checked");
+                }
+            });
+        }
+    },
+});
+// 디바이스 체크
+const device = {
+    check: function () {
+        isDevice.check();
+    },
+};
+const isDevice = {
+    window: () => /windows/i.test(navigator.userAgent),
+    mac: () => /macintosh/i.test(navigator.userAgent),
+    chrome: () =>
+        /chrome/i.test(navigator.userAgent) &&
+        !/edge/i.test(navigator.userAgent) &&
+        !/opr/i.test(navigator.userAgent),
+    firefox: () => /firefox/i.test(navigator.userAgent),
+    opera: () => /opera|OPR/i.test(navigator.userAgent),
+    safari: () =>
+        /safari/i.test(navigator.userAgent) &&
+        !/chrome/i.test(navigator.userAgent) &&
+        !/opr/i.test(navigator.userAgent),
+    edge: () => /edge/i.test(navigator.userAgent),
+    msie: () => /rv:11.0|msie/i.test(navigator.userAgent),
+    ie11: () => /rv:11.0/i.test(navigator.userAgent),
+    ie10: () => /msie 10.0/i.test(navigator.userAgent),
+    ie9: () => /msie 9.0/i.test(navigator.userAgent),
+    ie8: () => /msie 8.0/i.test(navigator.userAgent),
+    android: () => /android/i.test(navigator.userAgent),
+    ios: () => /iphone|ipad|ipod/i.test(navigator.userAgent),
+    any: function () {
+        return this.window() || this.mac();
+    },
+    check: function () {
+        const html = document.documentElement;
+        Object.entries(this).forEach(([key, func]) => {
+            if (
+                typeof func === "function" &&
+                key !== "check" &&
+                key !== "any" &&
+                func()
+            ) {
+                html.classList.add(key);
+            }
+        });
+    },
+};
--- tc-project/src/pages/login/LoginEasy.vue
+++ tc-project/src/pages/login/LoginEasy.vue
@@ -1,11 +1,144 @@
 <template>
-    <div>
-        <h1>login Page</h1>
+    <div class="inner">
+        <!-- form-styleBox 추가 -->
+        <div class="form-styleBox">
+            <!-- 탭 -->
+
+            <div class="form-wrap login-form">
+                <div class="form-row">
+                    <p class="lbl-only">아이디</p>
+                    <!-- input component -->
+                    <div class="input-box textType-2">
+                        <label for="ip01" class="blind">아이디 입력</label>
+                        <div class="inline-flex">
+                            <input
+                                id="ip01"
+                                type="text"
+                                title=""
+                                placeholder="아이디 입력"
+                                aria-label="입력하세요"
+                            />
+                            <div class="input-etc">
+                                <button
+                                    type="button"
+                                    class="btn input-del"
+                                    aria-label="입력항목 초기화"
+                                ></button>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- input component -->
+                </div>
+                <div class="form-row">
+                    <p class="lbl-only">비밀번호</p>
+                    <!-- input component -->
+                    <div class="input-box textType-2">
+                        <div class="inline-flex">
+                            <label for="ip02" class="blind"
+                                >비밀번호 입력</label
+                            >
+                            <input
+                                id="ip02"
+                                type="password"
+                                title=""
+                                placeholder="비밀번호 입력"
+                                aria-label="입력하세요"
+                            />
+                            <div class="input-etc">
+                                <button
+                                    type="button"
+                                    class="btn input-del"
+                                    aria-label="입력항목 초기화"
+                                ></button>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- input component -->
+                </div>
+                <div class="form-row">
+                    <button
+                        type="button"
+                        class="btn btn-primary btn-mid w-full"
+                    >
+                        로그인
+                    </button>
+                    <div class="align-right">
+                        <router-link to="/login/findId" class="btn-search-id"
+                            >아이디/비밀번호 찾기</router-link
+                        >
+                    </div>
+                </div>
+            </div>
+
+            <hr class="hr-divide ml-m24 mr-m24" />
+
+            <div class="tit-form mt-32">간편로그인</div>
+
+            <div class="easy-login-icons last-3" ref="easyLoginIcons">
+                <!-- 
+            last-마지막 li 인덱스 추가
+            ex) 
+              last-1 : 카카오
+              last-2 : 네이버
+              last-3 : 구글
+              last-4 : 애플
+          -->
+                <ul>
+                    <li
+                        v-for="(btn, index) in btns"
+                        :key="index"
+                        :class="btn.class"
+                    >
+                        <a href="#" class="btn-login circle" :class="btn.name">
+                            <strong class="blind">{{ btn.label }}</strong>
+                        </a>
+                        <span
+                            v-if="index === lastNumber - 1"
+                            class="last-loginAccount"
+                            >마지막에 로그인한 계정이에요.</span
+                        >
+                    </li>
+                </ul>
+            </div>
+            <!-- // 탭 -->
+
+            <div class="bottom-fixed">
+                <div class="btn-wrap">
+                    <div class="flex">
+                        <button type="button" class="btn btn-gray">
+                            회원가입
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- //form-styleBox 추가 -->
     </div>
 </template>
+<script setup>
+import { ref, onMounted } from "vue";
+import { commonUi } from "@/main";
 
-<script>
-export default {
-    name: "Home",
+onMounted(() => {
+    commonUi.init();
+    extractLastNumber();
+});
+
+const btns = [
+    { class: "btn-login-group", name: "kakao", label: "카카오" },
+    { class: "btn-login-group", name: "naver", label: "네이버" },
+    { class: "btn-login-group", name: "google", label: "구글" },
+    { class: "btn-login-group", name: "apple", label: "애플" },
+];
+
+const lastNumber = ref(0);
+const easyLoginIcons = ref(null);
+
+const extractLastNumber = () => {
+    const className = easyLoginIcons.value.className;
+    const match = className.match(/last-(\d+)/);
+    if (match) {
+        lastNumber.value = parseInt(match[1], 10);
+    }
 };
 </script>
